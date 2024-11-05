a987d367a760831bef16b96ded4f0fd6921695ce
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-11-05 12:15:20 +0530

    Add project files.

---
+++ Pattern54321/pattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//output of this program is 
+
+
+/*
+   *****
+   ****
+   ***
+   **
+   *
+   
+   
+   */
+namespace Pattern54321
+{
+     class Pattern
+    {
+        public static void Main(String[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j=i; j <= 5; j++)
+                {
+                    Console.Write("*");
+
+                }
+               
+                         
+                Console.WriteLine();
+            }
+        }
+    }
+}
